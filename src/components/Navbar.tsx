be5928a1e34d770b20cb3003002d475293255a46
felipecepluki <felipecepluki@gmail.com>
2022-12-15 15:45:17 -0300

    Add React Scroll Effect

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,52 @@
+import { Link } from "react-scroll";
+
 export default function Navbar() {
   return (
     <div className="w-screen flex items-center justify-evenly p-2">
-      <a className="font-unbounded text-lg text-white">Home</a>
-      <a className="font-unbounded text-lg text-white">Sobre</a>
-      <a className="font-unbounded text-lg text-white">Portfólio</a>
-      <a className="font-unbounded text-lg text-white">Projetos</a>
+      <Link
+        activeClass="active"
+        to="initial"
+        spy={true}
+        smooth={true}
+        offset={50}
+        duration={500}
+        className="font-unbounded cursor-pointer text-lg text-white"
+      >
+        Home
+      </Link>
+      <Link
+        activeClass="active"
+        to="about"
+        spy={true}
+        smooth={true}
+        offset={50}
+        duration={500}
+        className="font-unbounded cursor-pointer text-lg text-white"
+      >
+        Sobre
+      </Link>
+      <Link
+        activeClass="active"
+        to="portfolio"
+        spy={true}
+        smooth={true}
+        offset={50}
+        duration={500}
+        className="font-unbounded cursor-pointer text-lg text-white"
+      >
+        Portfólio
+      </Link>
+      <Link
+        activeClass="active"
+        to="projects"
+        spy={true}
+        smooth={true}
+        offset={50}
+        duration={500}
+        className="font-unbounded cursor-pointer text-lg text-white"
+      >
+        Projetos
+      </Link>
     </div>
   );
 }
